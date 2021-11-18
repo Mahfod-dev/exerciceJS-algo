4d74446e4d2f6783b44078ad646232be8fbb4ea6
Mahfod-dev <mahfod.dev@gmail.com>
2021-11-18 18:28:22 +0100

    first exercice algo

---
+++ starter/reverseString/index.js
@@ -0,0 +1,13 @@
+//Objectif : donner une chaine de caractere qui retourne une nouvelle chaine de caractere qui change l'ordre des caracteres.Une aide dispo dans le index.html
+
+
+//Goal : give a string , return a new string with reversed order characters.tip into index.html
+
+//Examples 
+
+// "bonjour" = "ruojnob"
+// "hello" = "olleh"
+
+function reverseStr(str) {
+    
+}
